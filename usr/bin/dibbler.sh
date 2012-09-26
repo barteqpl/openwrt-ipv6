f97b20023a177d28af7c7ca787e52996eb4bebba
B N <barteq@kretyn.com>
2012-09-27 01:42:57 +0200

    added action logging

--- usr/bin/dibbler.sh
+++ usr/bin/dibbler.sh
@@ -5,9 +5,9 @@ IFACE="`uci get dibblerclient.@interface[0].downlink_prefix_ifaces`"
 if [ -n "`uci get dibblerserver.@interface[0].pool|sed -e 's/\///'`" ]; then
 
 	if [ "$1" = add ] || [ "$1" = update ]; then
-		
-		if [ -n "$PREFIX1" ] && [ -n "$PREFIX1LEN" ]; then
 			
+		if [ -n "$PREFIX1" ] && [ -n "$PREFIX1LEN" ]; then
+		[ "$1" = add ] && logger -t dibblerclient "new prefix added - $PREFIX1/$PREFIX1LEN to interface $IFACE"
 			ip -6 addr add $PREFIX1/$PREFIX1LEN dev $IFACE
 						
 			uci set dibblerserver.@interface[0].pool="$PREFIX1/$PREFIX1LEN"
@@ -20,6 +20,7 @@ if [ -n "`uci get dibblerserver.@interface[0].pool|sed -e 's/\///'`" ]; then
 	fi
 	
 	if [ "$1" = delete ]; then
+	logger -t dibblerclient "prefix $PREFIX1/$PREFIX1LEN removed. Stopping dibbler-server."
 		uci set dibblerserver.@interface[0].pool=""
 		uci commit
 		/etc/init.d/dibblerserver stop
