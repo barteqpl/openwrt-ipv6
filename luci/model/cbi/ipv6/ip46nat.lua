26b9a3ebf55d5d79d3fa2182c467fe801ef84a05
B N <barteq@kretyn.com>
2012-10-14 00:25:47 +0200

    gui support for all module parameters. display status when loaded

--- luci/model/cbi/ipv6/ip46nat.lua
+++ luci/model/cbi/ipv6/ip46nat.lua
@@ -1,7 +1,7 @@
 --[[
 LuCI - Lua Configuration Interface
 
-Copyright 2011 Bartosz Nowicki <barteq@kretyn.com>
+Copyright 2012 Bartosz Nowicki <barteq@kretyn.com>
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -9,22 +9,80 @@ You may obtain a copy of the License at
 
 http://www.apache.org/licenses/LICENSE-2.0
 
-$Id: 6rd.lua 1 2011-04-13 20:22:36Z jow $ 
-
 ]]--
 
+local fs = require "nixio.fs"
 
 m = Map ("ip46nat", "IP46NAT module", "Experimental ip46nat module")
 s = m:section (TypedSection, "ip46nat","IP46NAT module configuration", nil)
 
-v4addr = s:option(Value, "v4addr", translate("IPv4 network address"), translate("Defines IPv4 subnet that will be translated to IPv6 traffic")) -- gets info about username 
+v4addr = s:option(Value, "v4addr", translate("IPv4 network address"), translate("Defines IPv4 subnet that will be translated to IPv6 traffic")) 
 v4addr.datatype = "ip4addr"
 v4addr.rmempty = false
 
-prefixlan = s:option (Value, "prefixlan", translate("LAN IPv6 prefix"), translate("IPv6 prefix for LAN side of the router"))
+v4masklen = s:option (Value, "v4masklen", translate("IPv4 mask length"), nil)
+v4masklen.datatype = "ip4prefix"
+v4masklen.default = 24
+v4masklen.optional = true
+
+v4offset = s:option (Value, "v4offset", translate("IPv4 offset"), translate("IPv4 offset in bits (0-95)"))
+v4offset.datatype = "uinteger"
+v4offset.optional = true 
+
+prefixlan = s:option (Value, "prefixlan", translate("LAN IPv6 prefix"), translate("IPv6 prefix for the LAN side of the router"))
 prefixlan.rmempty = false
 
-prefixwan = s:option (Value, "prefixwan", translate("WAN IPv6 prefix"), translate("IPv6 prefix for WAN side of the router"))
+prefixwan = s:option (Value, "prefixwan", translate("WAN IPv6 prefix"), translate("IPv6 prefix for the WAN side of the router"))
 prefixwan.rmempty = false
+
+v6prefixlen = s:option (Value, "v6prefixlen", translate("IPv6 prefix length"), nil)
+v6prefixlen.datatype = "ip6prefix"
+v6prefixlen.optional = true
+
+
+debug = s:option (ListValue, "debug", "Debug level")
+debug:value("0","0 - Disabled (default)")
+debug:value("1","1 - Prints matched packets only")
+debug:value("2","2 - Prints all packets (use with caution!)")
+debug.default = 0
+debug.optional = true
+
+--local file = "/proc/ip46nat/stats"
+--ip46stats = fs.readfile(file)
+--if ip46stats then
+
+local param_values = fs.readfile("/proc/ip46nat/params")
+
+state = s:option (DummyValue, "_state", translate("Module state"), nil)
+if param_values then
+	state.value = "Enabled"
+	params = s:option (TextValue, "_params", translate("Module parameters"), nil)
+	params.wrap = true
+	params.rows = 10
+	function params.cfgvalue(self, section)
+		return param_values
+	end
+	        
+else 
+	state.value = "Disabled"
+end
+
+if luci.sys.init.enabled('ip46nat') then 
+	btn = s:option(Button, "_btn", translate("Disable"), nil)
+	btn.inputstyle = "remove"
+	function btn.write()
+		luci.sys.init.disable('ip46nat')
+		luci.sys.call('/etc/init.d/ip46nat stop')
+		luci.http.header("refresh","1")
+	end
+else
+	btn = s:option(Button, "_btn", translate("Enable"), nil)
+	btn.inputstyle = "apply"
+	function btn.write()
+		luci.sys.init.enable('ip46nat')
+		luci.sys.call('/etc/init.d/ip46nat start')
+		luci.http.header("refresh","1")
+	end
+end
 
 return m
