cd0adf5acda19c5cdaf931fbd0382d53de015155
B N <barteq@kretyn.com>
2012-11-24 18:29:57 +0100

    New dibbler.sh with separated aftr from PD. Also changed default log mode value for dibbler.

--- usr/bin/dibbler.sh
+++ usr/bin/dibbler.sh
@@ -1,12 +1,50 @@
 #!/bin/sh
 
-# (c) 2012 - barteq.info
+# Automatic DS-Lite and PD delegation script
+# (c) 2012 - Bartosz Nowicki <barteqpl@gmail.com>
 
 IFACE="`uci get dibblerclient.@interface[0].downlink_prefix_ifaces`"
 
-if [  $(/etc/init.d/dibblerserver enabled && echo 1) ]; then
-# rise an action only if dibbler-server service is enabled
+# DS-LITE options
+if [ "$1" = add ] || [ "$1" = update ]; then
+	
+	# automatically sets up AFTR tunnel if recieved proepr AFTR address, dslite is allowed and enabled
+	if [ -n "$SRV_OPTION64" ] && [ `uci get dslite.@dslite[0].automatic` -eq 1 ] && [ $(/etc/init.d/dslite enabled && echo 1) ]; then
+	
+		# dirty but working. Better switch to nslookup, cause ping fails in case ds-lite enabled and no connection established
+		aftr_ip="`ping -6 -c 1 -w 1 -W 1 -q $SRV_OPTION64| head -n1 | sed -e 's/.*\ (\(.*\))\:.*/\1/'`"
+		logger -t dibblerclient "Received AFTR - $SRV_OPTION64 ($aftr_ip)"
 
+		if [ -z "`grep dslite0 /proc/net/dev`" ]; then
+			logger -t dibblerclient "No active AFTR tunnel found. Starting"
+			/etc/init.d/dslite start
+		
+		# do nothing in case tunnel is up and aftr IP remains the same
+		# if not assign new one and restart tunnel
+		else 
+			if [ "`uci get dslite.@dslite[0].aftr`" != $aftr_ip ]; then
+				uci set dslite.@dslite[0].aftr=$aftr_ip
+				uci commit
+				logger -t dibblerclient "Dslite module restarted"
+				/etc/init.d/dslite restart
+			fi
+		fi
+	fi
+fi
+
+if [ "$1" = delete ]; then		
+	if [ $(/etc/init.d/dslite enabled && echo 1) ]; then
+			
+			logger -t dibblerclient "Removing AFTR tunnel"
+			/etc/init.d/dslite stop
+	fi
+fi
+
+# Options related to automatic dibbler-serverconfiguration (Prefix Delegation)
+# rise an action only if dibbler-server service is enabled
+if [  $(/etc/init.d/dibblerserver enabled && echo 1) ]; then
+
+	# starts only if pool filed is empty
 	if [ -n "`uci get dibblerserver.@interface[0].pool|sed -e 's/\///'`" ]; then
 
 		if [ "$1" = add ] || [ "$1" = update ]; then
@@ -20,19 +58,6 @@ if [  $(/etc/init.d/dibblerserver enabled && echo 1) ]; then
 			
 				ip -6 addr add $PREFIX1/$PREFIX1LEN dev $IFACE
 			
-				# automatically sets aftr IP if allowed, enabled and both server and client supports this option 	
-				if [ -n "$SRV_OPTION64" ] && [ `uci get dslite.@dslite[0].automatic` -eq 1 ] && [ $(/etc/init.d/dslite enabled && echo 1) ]; then
-					aftr_ip="`ping -6 -c 1 -w 1 -W 1 -q $SRV_OPTION64| head -n1 | sed -e 's/.*\ (\(.*\))\:.*/\1/'`"
-					
-					logger -t dibblerclient "Received AFTR - $SRV_OPTION64 ($aftr_ip)"
-					if [ "`uci get dslite.@dslite[0].aftr`" != $aftr_ip ]; then
-						uci set dslite.@dslite[0].aftr=$aftr_ip
-						uci commit
-						logger -t dibblerclient "Dslite module restarted"
-						/etc/init.d/dslite restart
-					fi
-				fi
-				
 				uci set dibblerserver.@interface[0].pool="$PREFIX1/$PREFIX1LEN"
 				uci commit
 				/etc/init.d/dibblerserver restart
@@ -46,7 +71,6 @@ if [  $(/etc/init.d/dibblerserver enabled && echo 1) ]; then
 		fi
 	
 		if [ "$1" = delete ]; then
-		
 			logger -t dibblerclient "prefix $PREFIX1/$PREFIX1LEN removed. Stopping dibbler-server."
 			uci set dibblerserver.@interface[0].pool=""
 			uci commit
